4df4075236be9053c35e6aee078e455fefb3db4f
Ranica10 <77024134+Ranica10@users.noreply.github.com>
2023-10-04 20:09:15 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,12 +1,16 @@
 import streamlit as st
 
+# SET THE TITLE OF THE CURRENT OPEN PAGE
 st.set_page_config(page_title="Home", layout="wide")
+# WRITE A HEADER AND SOME TEXT
 st.header("Welcome!")
 st.write("This website is your one-stop destination for all things finance, technology, and innovation. Whether you're looking to optimize your budget, or explore the world of credit cards, we've got you covered!!")
 
+# CREATE A DIVIDER
 st.divider()
 
 st.subheader("Services we offer:")
+# USE STREAMLITS BUILT IN MARKDOWN FUNCTION TO ADD BULLET POINTS
 st.markdown(
 """
 - Budgeting Calculator
