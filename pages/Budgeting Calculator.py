a61cdc476dac678bfa558eee79836ddc4429096a
Ranica10 <77024134+Ranica10@users.noreply.github.com>
2023-10-04 20:09:38 -0400

    Update Budgeting Calculator.py

--- pages/Budgeting Calculator.py
+++ pages/Budgeting Calculator.py
@@ -1,30 +1,38 @@
+# IMPORT ALL THE NECESSARY LIBRARIES
 import streamlit as st
 import matplotlib.pyplot as plt
 
+# SET THE TITLE OF THE CURRENT OPEN PAGE
 st.set_page_config(page_title="Budgeting Calculator", layout="wide")
 st.header("Budgeting Calculator")
 st.write("Embark on a journey of financial planning via the form below to help you take control of your finances!!")
 
+# CREATE A VARIABLE TO CHECK IF THE INPUTS ARE FILLED IN TO NOT RUN THE PI CHART ON LOAD (CREATES AN ERROR)
 display_chart_var = False
 
 def display_chart(mortgage, rent, maintenance, internet, utilities, carloan, gas, insurance, 
                        repairs, tuition, studloan, supplies, groceries, entertainment, clothing, medical, other):
+    # SPLIT THE USER INPUTS INTO FOUR CATEGORIES WITH THEIR CORROSPONDING SUMS
     housing_expenses = mortgage + rent + maintenance + internet + utilities
     transportation_expenses = carloan + gas + insurance + repairs
     educational_expenses = tuition + studloan + supplies
     food_personal_expenses = groceries + entertainment + clothing + medical + other
 
-    # Data for the pie chart
+    # SAVE THE DATA FOR THE PI CHART IN A VARIABLE AS A LIST
     values = [housing_expenses, transportation_expenses, educational_expenses, food_personal_expenses]
-
+    # SAVE THE LABELS (CATERGORIES) FOR THE PI CHART AS A LIST
     labels = ['Housing Expenses', 'Transportation Expenses', 'Educational Expenses', 'Food and Personal Expenses']
 
+    # SET THE GLOBAL VARIABLE AS TRUE TO TELL THE PROGRAM THAT THE INPUTS WERE ALL FILLED IN
     display_chart_var = True
 
+    # RETURN THE DATA, LABELS, AND GLOBAL VARIABLE
     return values, labels, display_chart_var
 
 with st.form("user_inputs"):
+    # USE STREAMLIT BUILT IN FUNCTION TO SPILT THE FORM INTO THE FOUR CATEGORIES
     st.subheader("Monthly Income")
+    # TAKE IN THE USER INPUTS THROUGH THE FORM ELEMENT AND SAVE THEM TO THEIR CORROSPONDING VARIABLES
     salary = st.number_input("Salary", min_value = float(0), value=float(0))
     otherincome = st.number_input("Other Income", min_value = float(0), value=float(0))
 
@@ -60,9 +68,12 @@ with st.form("user_inputs"):
     medical = st.number_input("Medical", min_value = float(0), value=float(0))
     other = st.number_input("Other Expenses", min_value = float(0), value=float(0))
 
+    # CREATE A SUBMIT BUTTON
     submit = st.form_submit_button()
 
+    # IF THE SUBMIT BUTTON WAS CLICKED, RUN THE DISPLAY_CHART FUNCTION ABOVE TO GET THE PI CHART DATA AND LABELS
     if submit:
+        # SAVE THE OUTPUTS TO VARIABLES TO BE ACCESSED OUTSIDE THE FUNCTIONS
         values, labels, display_chart_var = display_chart(mortgage, rent, maintenance, internet, utilities, carloan, gas, insurance, 
                        repairs, tuition, studloan, supplies, groceries, entertainment, clothing, medical, other)
 
@@ -70,29 +81,33 @@ st.header("Results:")
 
 st.divider()
 
+# CHECKS IF THE VARIABLE IS TRUE (SET IF THE USER INPUTTED VALUES INTO THE FORM), AND THEN DISPLAYS THE CHART USING THE DATA AND LABELS
 if display_chart_var:
     st.subheader("Monthly Expenses by Category:")
 
-    # Plotting the pie chart
+    # PLOTTING THE PI CHART AS A WINDOW THAT CONTAINS THE PLOT
     fig, ax = plt.subplots(figsize=(8, 8))
 
-    # The percentages will be set to one significant digit using autopct
+    # THE PERCENTAGES IN THE CHART WILL BE SET TO ONE SIGNIFICANT DIGIT USING AUTOPCT
     ax.pie(values, labels=labels, autopct='%1.1f%%', startangle=140)
 
-    # Display the chart using st.pyplot()
+    # DISPLAY THE CHART USING MATHPLOTLIB BUILT IN FUNCTION
     st.pyplot(fig)
     st.divider()
 
     st.subheader("Net Income")
 
+    # CALCULATE THE NET INCOME OF THE USER
     netincome = salary + otherincome
 
-    # Go through every expenses catogory and subtract it from the salary + other income sources
+    # GO THROUGH ALL THE EXPENSES CATEGORIES AND SUBTRACT THEIR SUMS FROM THE SALARY AND OTHER INCOME SOURCES
     for i in range(len(values)):
         netincome -= values[i]
 
+    # DISPLAY THE NET INCOME TO THE USER
     st.write("$" ,str(float(netincome)))
 
+    # ADDS A PERSONALIZED MESSAGE BASED ON WHETHER THE USER IS ABOVE OR BELOW THEIR BUDGET
     if (netincome > 0):
         st.write("Looks like you're above your budget. Good Job!!")
     else:
@@ -101,6 +116,8 @@ if display_chart_var:
     st.divider()
 
 st.header("Budget Improvement Tips")
+
+# USE STREAMLITS BUILT IN MARKDOWN FUNCTION TO ADD BULLET POINTS
 st.markdown(
 """
 Managing a budget and staying within it is crucial for financial stability. If you find yourself going over budget, here are some tips and tricks to help you get back on track:
